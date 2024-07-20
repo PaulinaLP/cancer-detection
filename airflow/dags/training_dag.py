14d6fd18345c91927bf8abb86f9a88facc45615f
PaulinaPyzel <77441300+PaulinaLP@users.noreply.github.com>
2024-07-20 15:59:32 +0000

    modify dags

--- airflow/dags/training_dag.py
+++ airflow/dags/training_dag.py
@@ -16,21 +16,41 @@ default_args = {
 def save_csv_file(ti):
     df_train, df_test = ti.xcom_pull(task_ids='read_csv_file')  
     example=df_train.head(10)
-    example.to_csv (os.path.join(os.getcwd(),"input/example-metadata.csv"))
+    example.to_csv (os.path.join("/opt/airflow/output//example-metadata.csv"))
 
+
+def hello ():       
+    with open("/opt/airflow/output/file.txt", 'w') as file:        
+        file.write("hello")   
+
+
+def read_file():
+    file_path = "/opt/airflow/output/file.txt"
+    with open(file_path, 'r') as file:
+            content = file.read()
+            print(f"File content: {content}")
+           
 
 with DAG(
-    dag_id = 'training_pipeline',
+    dag_id = 'training_pipeline_2',
     description = 'Running a Python pipeline for training',
     default_args = default_args,
     start_date = days_ago(1),
     schedule_interval = '@once',
     tags = ['python', 'transform', 'pipeline']
 ) as dag:    
+    hello = PythonOperator(
+        task_id='hello',
+        python_callable= hello
+    )
+    read_file = PythonOperator(
+        task_id='read_file',
+        python_callable= read_file
+    )
     read_csv_file = PythonOperator(
         task_id='read_csv_file',
         python_callable= ingest_data,
-        op_kwargs={'path': os.path.join(os.getcwd(), "input/")}
+        op_kwargs={'path':  "/opt/airflow/input/"}
     )
     save_csv_file = PythonOperator(
         task_id='save_csv_file',
@@ -38,4 +58,4 @@ with DAG(
     )   
 
     
-read_csv_file >> save_csv_file
+hello >> read_file >> read_csv_file >> save_csv_file
--- setup/airflow.md
+++ setup/airflow.md
@@ -1,6 +1,15 @@
 https://airflow.apache.org/docs/apache-airflow/stable/howto/docker-compose/index.html
-
 First you have to download docker compose file.
-curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.9.2/docker-compose.yaml'
+urlc -LfO 'https://airflow.apache.org/docs/apache-airflow/2.9.2/docker-compose.yaml'
+You must change the volumes part if you want export / import files.
+Create airflow directory, leave the file there and cd to this directory.
 Then run this command on your terminal.
 docker compose up airflow-init
+Then run:
+docker compose up
+Wait till webserver and scheduler are up. You can open another terminal and do docker ps.
+There should be 6 containers up and healthy.
+If you made any mistake and want to delete the containers you should.
+docker stop $(docker ps -a -q)
+and then
+docker rm $(docker ps -a -q)
