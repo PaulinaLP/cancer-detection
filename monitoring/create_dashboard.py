8184d70d52bf9206be13b52a6d2e3e90ba072248
PaulinaPyzel <77441300+PaulinaLP@users.noreply.github.com>
2024-07-27 15:15:36 +0000

    modified:   monitoring/create_dashboard.py 	modified:   setup/evidently.md

--- monitoring/create_dashboard.py
+++ monitoring/create_dashboard.py
@@ -4,7 +4,7 @@ from evidently.report import Report
 from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric, ColumnQuantileMetric, ColumnSummaryMetric
 from evidently.metric_preset import DataDriftPreset, DataQualityPreset
 from evidently.ui.workspace import Workspace
-from evidently.ui.dashboards import DashboardPanelCounter, DashboardPanelPlot, CounterAgg, PanelValue, PlotType, ReportFilter
+from evidently.ui.dashboards import DashboardPanelCounter, DashboardPanelDistribution, DashboardPanelPlot, CounterAgg, PanelValue, PlotType, ReportFilter
 from evidently.renderers.html_widgets import WidgetSize
 from datetime import datetime
 
@@ -22,13 +22,14 @@ column_mapping = ColumnMapping(
 )
 
 ws = Workspace("workspace")
-project = ws.create_project("Cancer Detection 3")
+project = ws.create_project("Cancer Detection Dashboard 2")
 project.description = "This dashboard compares raw data from reference df and incoming data."
 project.save()
 
 regular_report = Report(
         metrics=[       
             DataQualityPreset(), 
+            DataDriftPreset(),
             ColumnQuantileMetric(column_name='tbp_lv_area_perim_ratio', quantile=0.2),
             ColumnQuantileMetric(column_name='age_approx', quantile=0.5)
         ],
@@ -46,24 +47,8 @@ project.dashboard.add_panel(
     DashboardPanelCounter(
         filter=ReportFilter(metadata_values={}, tag_values=[]),
         agg=CounterAgg.NONE,
-        title="Cancer Detection data dashboard"
+        title="Cancer Detection data dashboard 2"
     )
-)
-
-project.dashboard.add_panel(
-    DashboardPanelPlot(
-        filter=ReportFilter(metadata_values={}, tag_values=[]),
-        title="Inference Count",
-        values=[
-            PanelValue(
-                metric_id="DatasetSummaryMetric",
-                field_path="current.number_of_rows",
-                legend="count"
-            ),
-        ],
-        plot_type=PlotType.BAR,
-        size=WidgetSize.HALF,
-    ),
 )
 
 project.dashboard.add_panel(
@@ -82,20 +67,17 @@ project.dashboard.add_panel(
     ),
 )
 
-project.dashboard.add_panel(
-    DashboardPanelPlot(
-        filter=ReportFilter(metadata_values={}, tag_values=[]),
-        title="Age Quantile",
-        values=[
-            PanelValue(
-                metric_id="ColumnQuantileMetricResultc:age_approx:num:0.5",                
-                field_path="current.value",
-                legend="Age 0.5 Quantile"
+p.dashboard.add_panel(
+        DashboardPanelDistribution(
+            title="Column Distribution: current",
+            filter=ReportFilter(metadata_values={}, tag_values=[]),
+            value=PanelValue(
+                field_path="ColumnDistributionMetric.fields.current",
+                metric_id="ColumnDistributionMetric",
+                metric_args={"column_name.name": "age_approx"},
             ),
-        ],
-        plot_type=PlotType.LINE,
-        size=WidgetSize.HALF,
-    ),
-)
+            barmode = HistBarMode.STACK
+        )
+    )
 
 project.save()
--- setup/evidently.md
+++ setup/evidently.md
@@ -2,3 +2,7 @@ cd monitoring
 install dependencies with pipenv
 pipenv shell
 you can see the dashboard with command: evidently ui
+
+if you need to reset the ui:
+lsof -i :8000
+kill -9 <pid>
